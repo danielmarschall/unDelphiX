db3db2e25d6fbdfc640e96b2cd14baa9e6fdeeeb
Daniel Marschall <info@daniel-marschall.de>
2024-04-13 21:34:36 +0200

    (Typo)

--- Source/DirectPlay.pas
+++ Source/DirectPlay.pas
@@ -21,7 +21,7 @@ interface
 {$INCLUDE DelphiXcfg.inc}
 
 {$IFNDEF UseDirectPlay} // Daniel Marschall 12.04.2024 Added to avoid Windows showing "This app requires DirectPlay"
-{$MESSAGE ERROR 'If you want to use DXPlay.pas, please enable the IFDEF UseDirectPlay in DelphiXcfg.inc'}
+{$MESSAGE ERROR 'If you want to use DirectPlay.pas, please enable the IFDEF UseDirectPlay in DelphiXcfg.inc'}
 {$ENDIF}
 
 {$MINENUMSIZE 4}
